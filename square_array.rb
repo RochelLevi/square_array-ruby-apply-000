57ff295e9664957febacd348993ab1b1e5379f9a
Rochel Levi <rochlevi@gmail.com>
2017-09-05 22:42:39 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -8,9 +8,7 @@ end
 
 
 def square_array(array)
-  new_array = []
-  array.collect do |elem|
-    new_array.push(elem * elem)
+  array.collect |elem|
+    (elem * elem)
   end
-  return new_array
 end
